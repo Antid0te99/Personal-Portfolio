ae73d9078b6d5c357193c136faa5aceecd593e1c
Antid0te99 <onlyedu999@gmail.com>
2025-09-24 17:33:11 +0545

    Initial commit: Personal Portfolio

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+app= Flask(__name__)
+# @app.route("/test")
+# def home():
+#     return render_template("test.html")
+@app.route("/main")
+def test():
+    return render_template("main.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+    
+if __name__=="__main__":
+    app.run(debug=True)
